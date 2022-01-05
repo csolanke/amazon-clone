1fd04bc491483c30032b17df2aeccdac7e5751b6
Chandrakant <chandrakant@Chandrakants-MacBook-Pro.local>
2022-01-04 22:25:19 -0500

    Added Styling for Header component

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import { ShoppingBasket } from '@material-ui/icons';
 
 function Header() {
     return (
@@ -11,37 +13,47 @@ function Header() {
 
             <div className='header_search'>
                 <input className='header_search_input'
-                type="text"
+                type="text" 
                 />
+                <SearchIcon className='header_searchIcon'/>
+                
             </div>
             
             <div className='header_nav'>
                 <div className='header_option'>
-                    <span className='header_OptionLineOne'>
+                    <span className='header_optionLineOne'>
                       Hello CK
                     </span>
-                    <span className='header_OptionLineTwo'>
+                    <span className='header_optionLineTwo'>
                       Sign In
                     </span>
                 </div>
 
                 <div className='header_option'>
-                <span className='header_OptionLineOne'>
+                <span className='header_optionLineOne'>
                       Returns
                     </span>
-                    <span className='header_OptionLineTwo'>
+                    <span className='header_optionLineTwo'>
                       & Orders
                     </span>
                 </div>    
                 
 
                 <div className='header_option'>
-                <span className='header_OptionLineOne'>
+                <span className='header_optionLineOne'>
                       Your
                     </span>
-                    <span className='header_OptionLineTwo'>
+                    <span className='header_optionLineTwo'>
                       Prime
                     </span>
+                </div>
+
+                <div className='header_optionBasket'>
+                  <ShoppingBasket/>
+                  <span className='header_optionLineTwo header_basketCount'>
+                   0
+                  </span>
+
                 </div>
 
             </div>
---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import './Home.css';
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home'>
+
+           <div className='home__container'>
+            <img className='home__image' src='https://images-na.ssl-images-amazon.com/images/G/01/img18/home/2022/Q1/SSHeaders/HMT_Q42021_desk_ssheader_geometric_1500x550.jpg' alt=''>
+            </img>   
+           </div>
+
+           <div className='home__row'>
+               {/* product1 */}
+               {/* product 2 */}
+           </div>
+
+           <div className='home__row'>
+             {/* product1 */}
+             {/* product2 */}
+             {/* product3 */}
+           </div>
+
+           <div className='home__row'>
+              {/* product1 */}
+           </div>
+
+        </div>
+    )
+}
+
+export default Home
