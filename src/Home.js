0a05b7c2f3bd4b6a8711dee61cb8264666b099dd
Chandrakant <chandrakant@Chandrakants-MacBook-Pro.local>
2022-01-08 18:49:53 -0500

    Added styling to product component and rendered few products

--- src/Home.js
+++ src/Home.js
@@ -12,17 +12,25 @@ function Home() {
            </div>
 
            <div className='home__row'>
-               <Product />
+               <Product id={1} title='Rich Dad Poor Dad' price={30} rating={4}
+               imageSource='https://m.media-amazon.com/images/I/81bsw6fnUiL._AC_UY436_FMwebp_QL65_.jpg'/>
+               <Product id={2} title='The Psychology of Money' price={20} rating={3}
+               imageSource='https://m.media-amazon.com/images/I/51jRBz6Ug3L.jpg' />
+              <Product id={3} title='The Intelligent Investor' price='45' rating={5}
+              imageSource='https://m.media-amazon.com/images/I/919mmNCTaaL._AC_UY436_FMwebp_QL65_.jpg'/> 
            </div>
 
            <div className='home__row'>
-             {/* product1 */}
-             {/* product2 */}
-             {/* product3 */}
+           <Product id={4} title='You cant Hurt me' price='10' rating={5}
+              imageSource='https://m.media-amazon.com/images/I/81hwCOQ1F0L._AC_UY436_FMwebp_QL65_.jpg'/> 
+            <Product id={5} title='Two States' price='18' rating={3}
+              imageSource='https://m.media-amazon.com/images/I/714GGM7WK7L._AC_UY436_FMwebp_QL65_.jpg'/> 
            </div>
 
            <div className='home__row'>
-              {/* product1 */}
+           <Product id={6} title='SAMSUNG 50-Inch Class Crystal UHD AU8000 Series - 4K UHD HDR Smart TV with Alexa Built-in (UN50AU8000FXZA, 2021 Model)'
+                     price={400} rating={7}
+           imageSource='https://m.media-amazon.com/images/I/71LJJrKbezL._AC_UY436_FMwebp_QL65_.jpg'/>
            </div>
 
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({title,imageSource,price,rating}) {
     return (
         <div className='product'>
               <div className='product__info'>
-                   <p>The Psychology of money</p>
+                   <p>{title}</p>
                   <p className='product__price'> 
                       <small>$</small>
-                      <strong>20.00</strong>
+                      <strong>{price}</strong>
                   </p>
-             
 
                     <div className='product__rating'> 
-                       <p>🌟🌟🌟🌟</p>
+                       {Array(rating).fill()
+                       .map((_,i)=>(
+                        <p>🌟</p>
+                       ))}
+                      
                     </div>
               </div>
 
-                <img src='https://m.media-amazon.com/images/I/51jRBz6Ug3L.jpg'> 
+                <img src={imageSource}> 
                 </img>
 
                 <button>Add to Basket</button>
